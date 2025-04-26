c119d4a15792df7e855243ddcb224c611c3df37e
Arnob Roy <arnobroy600@gmail.com>
2025-04-27 00:46:11 +0600

    Add files via upload

---
+++ CP_Workshop/hw3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1.1.3. Write a program to enter two integer numbers and display their sum, difference and multiplication value.
+    int a, b;
+
+    cin >> a;
+    cin >> b;
+
+    cout << a + b << '\n';
+    cout << a - b << '\n';
+    cout << a * b << '\n';
+
+    return 0;
+
+}
