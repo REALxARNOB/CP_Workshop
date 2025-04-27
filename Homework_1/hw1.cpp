0b1a5662f92ff74f45154d6e02a2faa6ea039f05
Arnob Roy <arnobroy600@gmail.com>
2025-04-27 14:11:05 +0600

    Add files via upload

---
+++ Homework_1/hw1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //1.1.1. Write a program to take input and display an integer number, a fraction number, a character and a boolean.
+    int a;
+    float b;
+    char c;
+    bool d;
+
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    cin >> d;
+    
+    cout << a << '\n';
+    cout << b << '\n';
+    cout << c << '\n';
+    cout << d << '\n';
+
+    return 0;
+}
